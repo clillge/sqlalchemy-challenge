e42dac7226e403635a566c3469face810ec00f11
clillge <colelillge@gmail.com>
2023-12-04 22:08:24 -0500

    Added more to app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -57,9 +57,7 @@ def precipitation():
     #(i.e. retrieve only the last 12 months of data) to a dictionary 
     #using date as the key and prcp as the value.
     end_date = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
-    date = dt.datetime.strptime(end_date, '%Y-%m-%d')
-    date = date.date()
-    
+    date = dt.datetime.strptime(end_date, '%Y-%m-%d').date()
     start_date = date - dt.timedelta(days=365)
 
     final_year_data = session.query(measurement.date, measurement.prcp).filter(measurement.date >= start_date)
@@ -92,11 +90,56 @@ def tobs():
 
     #Query the dates and temperature observations of the 
     #most-active station for the previous year of data.
-    
-    #Return a JSON list of temperature observations for the previous year.
+    end_date = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
+    date = dt.datetime.strptime(end_date, '%Y-%m-%d').date()
+    start_date = date - dt.timedelta(days=365)
 
+    final_year_station_data = session.query(measurement.date, measurement.tobs)\
+    .filter(measurement.date >= start_date).filter(measurement.station == 'USC00519281')\
+    .all()
+
+    #Return a JSON list of temperature observations for the previous year.
+    dictionary = []
+    for date,tobs in final_year_station_data:
+        interim_dict = {}
+        interim_dict[date] = tobs
+        dictionary.append(interim_dict)
+
+    return jsonify(dictionary)
 
 #Min/Max/Avg
 
+# Return a JSON list of the minimum temperature, the average temperature, 
+# and the maximum temperature for a specified start or start-end range.
 
-session.close()
+@app.route('/api/v1.0/<start>')
+def start(start_date):
+# For a specified start, calculate TMIN, TAVG, and TMAX for 
+# all the dates greater than or equal to the start date.
+
+    first_date = session.query(measurement.date).order_by(measurement.date.asc()).first()[0]
+    last_date = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
+
+    if start_date >= first_date and start_date <= last_date:
+
+        return session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+        filter(measurement.date >= start_date).all()
+
+
+@app.route('/api/v1.0/<start>/<end>')
+def start_end(start_date, end_date):
+
+# For a specified start date and end date, calculate TMIN, TAVG, 
+# and TMAX for the dates from the start date to the end date, inclusive.
+
+    first_date = session.query(measurement.date).order_by(measurement.date.asc()).first()[0]
+    last_date = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
+
+    if start_date >= first_date and end_date <= last_date:
+
+        return session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+        filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
